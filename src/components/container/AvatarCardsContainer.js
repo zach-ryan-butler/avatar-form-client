67ac0175f972f4d87e65cdc20f8eaa2419f54cac
Zach Butler <zach.standley93@gmail.com>
2020-09-20 19:19:21 -0700

    handled logic for input form control, added use effect hooks to fetch from the api when value of input field changes

--- src/components/container/AvatarCardsContainer.js
+++ src/components/container/AvatarCardsContainer.js
@@ -1,20 +1,60 @@
 import React, { useEffect, useState } from 'react';
-import { getCharacters, getCharacterByAffiliation } from '../../services/avatarApi';
+import { getCharacters, getCharacterByName } from '../../services/avatarApi';
 import AvatarCardList from '../avatarCardList/AvatarCardList';
+import Header from '../header/Header';
+import AvatarForm from '../avatarForm/AvatarForm';
+
+const initialValues = {
+  name: ''
+}
 
 export default function AvatarCardsContainer() {
 
-  const [characters, setCharacters] = useState([])
-  
-  useEffect(() => {
-    getCharacters()
-      .then(res => setCharacters(res))
-  }, [])
+  const characters = [
+    {
+      name: 'Ashuna',
+      url: 'https://vignette.wikia.nocookie.net/avatar/images/9/9a/Ashuna.png/revision/latest?cb=20161026183558'
+    },
+    {
+      name: 'Azula',
+      url: 'https://vignette.wikia.nocookie.net/avatar/images/1/12/Azula.png/revision/latest?cb=20140905084941'
+    },
+    {
+      name: 'Aang',
+      url: 'https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003'
+    }
+  ]
 
-  getCharacterByAffiliation('Fire Nation')
-    .then(res => console.log(res))
+  // const [characters, setCharacters] = useState([])
+  const [values, setValues] = useState(initialValues);
+  
+  // useEffect(() => {
+  //   getCharacters()
+  //     .then(res => setCharacters(res))
+  // }, [])
+
+  // useEffect(() => {
+  //   if(values.name.length > 0) {
+  //     getCharacterByName(values.name)
+  //       .then(res => setCharacters(res))
+  //   } else {
+  //     getCharacters()
+  //       .then(res => setCharacters(res))
+  //   }
+  // }, [values.name])
+
+  const handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    setValues({
+      [name]: value
+    })
+  }
 
   return (
-    <AvatarCardList characters={characters} />
+    <>
+      <Header />
+      <AvatarForm handleInputChange={handleInputChange} values={values} />
+      <AvatarCardList characters={characters} />
+    </>
   )
 }
