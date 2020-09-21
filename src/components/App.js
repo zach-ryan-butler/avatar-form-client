3937e35fba7eebf88070ceed962febd7b02cd204
Zach Butler <zach.standley93@gmail.com>
2020-09-20 19:17:06 -0700

    removed avatarvardlist component and test array

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,7 @@
 import React from 'react';
-// import AvatarCardList from './avatarCardList/AvatarCardList';
 import AvatarCardsContainer from './container/AvatarCardsContainer';
 
 function App() {
-  // const characters = [
-  //   {
-  //     name: 'Ashuna',
-  //     url: 'https://vignette.wikia.nocookie.net/avatar/images/9/9a/Ashuna.png/revision/latest?cb=20161026183558'
-  //   },
-  //   {
-  //     name: 'Azula',
-  //     url: 'https://vignette.wikia.nocookie.net/avatar/images/1/12/Azula.png/revision/latest?cb=20140905084941'
-  //   },
-  //   {
-  //     name: 'Aang',
-  //     url: 'https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003'
-  //   },
-  //   {
-  //     name: 'Ashuna',
-  //     url: 'https://vignette.wikia.nocookie.net/avatar/images/9/9a/Ashuna.png/revision/latest?cb=20161026183558'
-  //   },
-  //   {
-  //     name: 'Azula',
-  //     url: 'https://vignette.wikia.nocookie.net/avatar/images/1/12/Azula.png/revision/latest?cb=20140905084941'
-  //   },
-  //   {
-  //     name: 'Aang',
-  //     url: 'https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003'
-  //   }
-  // ]
-
-
   return (
     <AvatarCardsContainer />
   );
