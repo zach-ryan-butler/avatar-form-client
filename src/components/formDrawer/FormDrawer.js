2056922801042fe4f00efa0dc9b0265b0eb5c9c3
Zach Butler <zach.standley93@gmail.com>
2020-09-21 20:27:06 -0700

    added todos for drawer component

--- src/components/formDrawer/FormDrawer.js
+++ src/components/formDrawer/FormDrawer.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+// Build out drawer
+// import input field into form
+// build out and import checkboxes for nation into drawer
+// make drawer mobile friendly
+
 export default function FormDrawer() {
-  
+  return (
+    <>
+    </>
+  )
 }
