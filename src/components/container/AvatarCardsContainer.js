a41e3cf0db3caa514e8f9a23fd9e9fe7f99cc891
Zach Butler <zach.standley93@gmail.com>
2020-09-21 20:26:16 -0700

    tested new filter functions

--- src/components/container/AvatarCardsContainer.js
+++ src/components/container/AvatarCardsContainer.js
@@ -3,9 +3,11 @@ import { getCharacters, getCharacterByName } from '../../services/avatarApi';
 import AvatarCardList from '../avatarCardList/AvatarCardList';
 import Header from '../header/Header';
 import AvatarForm from '../avatarForm/AvatarForm';
+import { filterByAffiliation, filterByName } from '../filter/Filter';
 
 const initialValues = {
-  name: ''
+  name: '',
+  nations: ['air']
 }
 
 export default function AvatarCardsContainer() {
@@ -24,10 +26,10 @@ export default function AvatarCardsContainer() {
       url: 'https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003'
     }
   ]
-
+  
   // const [characters, setCharacters] = useState([])
   const [values, setValues] = useState(initialValues);
-  
+
   // useEffect(() => {
   //   getCharacters()
   //     .then(res => setCharacters(res))
@@ -35,8 +37,8 @@ export default function AvatarCardsContainer() {
 
   // useEffect(() => {
   //   if(values.name.length > 0) {
-  //     getCharacterByName(values.name)
-  //       .then(res => setCharacters(res))
+  //     const filteredName = filterByName(values.name, characters)
+  //     setCharacters(filteredName);
   //   } else {
   //     getCharacters()
   //       .then(res => setCharacters(res))
@@ -46,6 +48,7 @@ export default function AvatarCardsContainer() {
   const handleInputChange = ({ target }) => {
     const { name, value } = target;
     setValues({
+      ...values,
       [name]: value
     })
   }
